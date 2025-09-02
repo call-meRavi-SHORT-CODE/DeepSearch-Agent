dd30fcbc952a15e026d76a33222623c4d227548c
call-meRavi-SHORT-CODE <ravikrishnaj25@gmail.com>
2025-09-02 21:48:00 +0530

    websearch tool added

--- app.py
+++ app.py
@@ -39,4 +39,20 @@ class State:
 
 
 
-    
+## Tool -- > websearch
+
+def tavily_serach(query):
+    tavily_client = TavilyClient(api_key=config.TAVILY_API_KEY)
+
+    return tavily_client.search(query,
+                                include_raw_content=True,
+                                max_results=3)
+
+# fuction to update System State with search results
+
+def update_state_with_search_results(results,idx,state):
+
+    for i in results['results']:
+        search = Search(url=i['url'],content=i['content'])
+        state.paragraphs[idx].research.search_history.append(search)
+
